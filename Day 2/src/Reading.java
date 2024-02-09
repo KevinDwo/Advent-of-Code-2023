316cd67d9f6c4a27e41b12c3f8e8e0eb2e0b136f
Kevin Dworzak <dworkevin@gmail.com>
2024-02-09 18:50:00 +0100

    Day 2 finished!

--- Day 2/src/Reading.java
+++ Day 2/src/Reading.java
@@ -3,77 +3,64 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Reading {
-    private final int red;
-    private final int green;
-    private final int blue;
 
     public Reading() {
-        red = 12;
-        green = 13;
-        blue = 14;
     }
     public void readTextFile(){
         String filePath = "C:/Users/dwork/OneDrive/Persönliche Projekte/Advent of Code 2023/Advent-of-Code-2023/Day 2/Input.txt";
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
-            int currentGameNumber = 1;
             int sum = 0;
 
             while ((line = reader.readLine()) != null) {
 
                 String currentSection;
-                boolean possible = true;
                 int sectionNumber = 0;
+                // red, green, blue
+                int[] highestNumbers = new int[3];
 
                 //Eine Zeile wird getestet
                 while (sectionNumber != -1){
+                    int[] currentNumbers;
                     try {
                         currentSection = line.substring(sectionNumber, line.indexOf(";", sectionNumber));
                     } catch (Exception e) {
                         break;
                     }
                     sectionNumber = line.indexOf(";", sectionNumber) + 1;
-                    if (!testPossible(currentSection)){
-                        possible = false;
-                        break;
+                    // red, green, blue
+                    currentNumbers = testPossible(currentSection);
+                    for (int i = 0; i < currentNumbers.length; i++){
+                        if (currentNumbers[i] > highestNumbers[i]){
+                            highestNumbers[i] = currentNumbers[i];
+                        }
                     }
                 }
-
-
-                if (possible){
-                    sum += currentGameNumber;
+                int power = 1;
+                for (int highestNumber : highestNumbers) {
+                    power *= highestNumber;
                 }
-
+                sum += power;
                 System.out.println(sum);
-                currentGameNumber++;
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public boolean testPossible(String line){
-        int value;
+    public int[] testPossible(String line){
+        int[] value = new int[3];
         if(line.contains("red")){
-            value = getNumber(line, line.indexOf("red"));
-            if(value > red){
-                return false;
-            }
-        }
-        if(line.contains("blue")){
-            value = getNumber(line, line.indexOf("blue"));
-            if(value > blue){
-                return false;
-            }
+            value[0] = getNumber(line, line.indexOf("red"));
         }
         if(line.contains("green")){
-            value = getNumber(line, line.indexOf("green"));
-            if(value > green){
-                return false;
-            }
+            value[1] = getNumber(line, line.indexOf("green"));
         }
-        return true;
+        if(line.contains("blue")){
+            value[2] = getNumber(line, line.indexOf("blue"));
+        }
+        return value;
     }
 
     public int getNumber(String line, int position){
